7e86fbbac18090d54684d5cc0da80f888b96c761
Adner16 <giovanni.renda16@gmail.com>
2022-01-13 17:46:28 +0100

    fix jsnack4

--- js/jsnack4.js
+++ js/jsnack4.js
@@ -1,10 +1,39 @@
 
-const numList = parseInt([1,2,1,4,1,6,1,8,1,10]);
+// ciclo FOR
+
+
+const numList = [1,2,3,4,5,6,7,8,9,10];
 let sum = 0; 
 
 for ( i = 0; i < numList.length; i++){
-    if (!(numList[i] %2)) {
+    if (i %2 != 0) {
         sum += numList[i];
     }
 }
+console.log(sum);
+
+
+// ciclo WHILE
+
+let i = 0;
+
+while (i < numList.length){
+    if (i %2 != 0) {
+        sum += numList[i];
+    }
+    i++;
+}
+console.log(sum);
+
+// ciclo DO 
+
+let i = 0;
+
+do{
+     if (i %2 != 0) {
+    sum += numList[i];
+}
+i++;
+} while (i < numList.length);
+
 console.log(sum);
