7a5320025db28417c2125ad0bce8d1b2e6f1073d
Adner16 <giovanni.renda16@gmail.com>
2022-01-14 15:50:36 +0100

    jsanck5+

--- js/jsnack5.js
+++ js/jsnack5.js
@@ -3,12 +3,17 @@ console.log('js ok');
 let listOne = [1,2,3,4,5];
 let listTwo = [1,2,3];
 
+
 if(listOne.length < listTwo.length){
+    for ( i = 0; i < listOne.length; i++){
    const randomItem = Math.floor(Math.random() * 5 + 1);
-    parseInt(listOne) += randomItem;
-    console.log(listOne)
+    listOne.push(randomItem);
+    console.table(listOne);
+    }
 } else {
+    for ( i = 0 ; i < listTwo.length; i++){
     const randomItem = Math.floor(Math.random() * 5 + 1);
-    listTwo += parseInt(randomItem);
-    console.log(listTwo);
+    listTwo.push (randomItem);
+    console.table(listTwo);
+    }
 }
